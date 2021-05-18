a115072bf2f5fb3e062e1f8f9cfab0760ea1dd8e
So Vengleab <sovengleab@gmail.com>
2021-05-18 18:58:47 +0700

    Week04 Change functional to class componet

--- week03/components/Item/index.js
+++ week03/components/Item/index.js
@@ -8,17 +8,40 @@ import {
 } from 'react-native';
 import ListItemContext from '../../context/ListItemContext';
 
-export default props => {
-  const { selectedItem, setSelectedItem } = React.useContext(ListItemContext);
-  const { item } = props;
-  return (
-    <TouchableOpacity onPress={e => setSelectedItem(item)}>
-      <View style={[styles.item, item === selectedItem && styles.active]}>
-        <Text>{props.item}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
+// export default props => {
+//   const { selectedItem, setSelectedItem } = React.useContext(ListItemContext);
+//   const { item } = props;
+//   return (
+//     <TouchableOpacity onPress={e => setSelectedItem(item)}>
+//       <View style={[styles.item, item === selectedItem && styles.active]}>
+//         <Text>{props.item}</Text>
+//       </View>
+//     </TouchableOpacity>
+//   );
+// };
+
+export default class Item extends React.Component {
+  // static contextType = ListItemContext;
+  render() {
+    // const { selectedItem, setSelectedItem } = this.context;
+    const { item } = this.props;
+    return (
+      <ListItemContext.Consumer>
+        {/* {contextProps => { */}
+        {({ setSelectedItem, selectedItem }) => {
+          return (
+            <TouchableOpacity onPress={e => setSelectedItem(item)}>
+              <View
+                style={[styles.item, item === selectedItem && styles.active]}>
+                <Text>{item}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      </ListItemContext.Consumer>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   item: {
