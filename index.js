8241d1f2d3d95ff6de6ef2d6f079fd01daa1edde
So Vengleab <sovengleab@gmail.com>
2021-05-18 19:05:31 +0700

    Week04 rename folder from week03 to week03_04

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './week03/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './week03_04/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
