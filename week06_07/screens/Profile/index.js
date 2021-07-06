6377b4427616d96c46aef64c3fc609736df3600f
So Vengleab <sovengleab@gmail.com>
2021-07-06 18:57:40 +0700

    Weeok09 clear navigation params

--- week06_07/screens/Profile/index.js
+++ week06_07/screens/Profile/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-paper';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import {
+  useNavigation,
+  useRoute,
+  useFocusEffect,
+} from '@react-navigation/native';
 
 const Profile = props => {
   //* INFO: we can get from props if it is top level component to screen;
@@ -9,6 +13,23 @@ const Profile = props => {
   // const navigation = props.navigation;
   const { params = {} } = useRoute(); // get from hook
   const navigation = useNavigation();
+
+  useFocusEffect(() => {
+    console.log('On focus');
+
+    return () => {
+      console.log('On blur');
+      navigation.setParams({ from: null, to: null });
+    };
+  });
+
+  useEffect(() => {
+    console.log('Component didmount');
+
+    return () => {
+      console.log('Component willUnmount');
+    };
+  });
   return (
     <View
       style={{
